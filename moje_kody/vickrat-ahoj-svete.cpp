576608aeb1049afd7317232f0b59dc7435ff8576
licmand <st026339@student.cichnovabrno.cz>
2025-09-15 14:17:41 +0200

    vickrat ahoj svete

---
+++ moje_kody/vickrat-ahoj-svete.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int nasobek;
+
+    printf("zadej kolikrat se ma vypsat hello world: ");
+    scanf("&d", nasobek);
+    
+    if (nasobek <= 0) {
+        printf("error lol");
+    }
+
+    else {
+        for (int i = 0; i < nasobek; i++) {
+            printf("Hello World\n");
+        }
+    }
+
+    
+    int keepalive;
+    scanf("%d", &keepalive);
+    return 0;
+}
