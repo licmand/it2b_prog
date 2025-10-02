69a81871c6ac349b3c982bb8e280f3411e417a58
licmand <st026339@student.cichnovabrno.cz>
2025-10-02 10:31:03 +0200

    debug volby.cpp

--- moje_kody/volby.cpp
+++ moje_kody/volby.cpp
@@ -4,9 +4,10 @@
 
 int main() {
     srand(time(NULL));
-    char strany[][14] = {"ANO", "SPOLU", "SPD", "STAN", "Piráti", "Motoristé", "Stačilo", "Jiné"};
+    char strany[][14] = {"ANO", "SPOLU", "SPD", "STAN", "Pirati", "Motorista", "Stacilo", "Jine"};
     float preference[] = {29.3, 20.5, 13.4, 11.1, 10, 6, 5.5, 4.2};
     int hlasy[] = {0,0,0,0,0,0,0,0};
+    float procenta[] = {0,0,0,0,0,0,0,0};
     int muze_volit = 8300000;
     int bude_volit_procent = (rand() % 30) + 51;
     int bude_volit = (muze_volit * bude_volit_procent) / 100;
@@ -15,7 +16,7 @@ int main() {
 
     printf("Bude volit: %d\n", bude_volit);
      
-    printf("Probiha hlasovani\n");
+    printf("Probiha hlasovani...\n\n");
 
     for (int i = 0; i < bude_volit; i++) {
         int r = rand() % 101; // číslo 0–100
@@ -48,15 +49,22 @@ int main() {
                 printf("\n\nnekde nastala chyba\n\n");
         }
     }
+    
+    for (int i = 0; i < 8; i++) {
+        procenta[i] = ((float)hlasy[i]/bude_volit)*100;
+        //printf("\nprocnta vygenerovane i=%d procenta: %.2f\n", i, procenta[i]);
+        printf("%s ziskalo %d hlasu a ma %.2f%%\n", strany[i], hlasy[i], procenta[i]);
 
-    printf("%s ziskalo %d hlasu a ma %.f %", strany[0], hlasy[0],((hlasy[0]/bude_volit)*100));
-    printf("%s ziskalo %d hlasu a ma %.f %", strany[1], hlasy[1],((hlasy[1]/bude_volit)*100));
-    printf("%s ziskalo %d hlasu a ma %.f %", strany[2], hlasy[2],((hlasy[2]/bude_volit)*100));
-    printf("%s ziskalo %d hlasu a ma %.f %", strany[3], hlasy[3],((hlasy[3]/bude_volit)*100));
-    printf("%s ziskalo %d hlasu a ma %.f %", strany[4], hlasy[4],((hlasy[4]/bude_volit)*100));
-    printf("%s ziskalo %d hlasu a ma %.f %", strany[5], hlasy[5],((hlasy[5]/bude_volit)*100));
-    printf("%s ziskalo %d hlasu a ma %.f %", strany[6], hlasy[6],((hlasy[6]/bude_volit)*100));
-    printf("%s ziskalo %d hlasu a ma %.f %", strany[7], hlasy[7],((hlasy[7]/bude_volit)*100));
+    }
+
+    /*printf("\n%s ziskalo %d hlasu a ma %.2f %\n", strany[0], hlasy[0], procenta[0]);
+    printf("%s ziskalo %d hlasu a ma %.2f %\n", strany[1], hlasy[1], procenta[1]);
+    printf("%s ziskalo %d hlasu a ma %.2f %\n", strany[2], hlasy[2], procenta[2]);
+    printf("%s ziskalo %d hlasu a ma %.2f %\n", strany[3], hlasy[3], procenta[3]);
+    printf("%s ziskalo %d hlasu a ma %.2f %\n", strany[4], hlasy[4], procenta[4]);
+    printf("%s ziskalo %d hlasu a ma %.2f %\n", strany[5], hlasy[5], procenta[5]);
+    printf("%s ziskalo %d hlasu a ma %.2f %\n", strany[6], hlasy[6], procenta[6]);
+    printf("%s ziskalo %d hlasu a ma %.2f %\n", strany[7], hlasy[7], procenta[7]);*/
 
     int keepalive;
     scanf("%d", &keepalive);
